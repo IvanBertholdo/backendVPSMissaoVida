f309f744a94abc874ea5db746c2efd4ed7d77748
Ivan Bertholdo <ivanbertholdo38@gmail.com>
2024-08-31 01:34:52 -0300

    Trocando para LocalHost

--- openAPI.js
+++ openAPI.js
@@ -13,7 +13,7 @@ const openApiDocs = {
         description: 'Find more info here'
       },
       servers: [
-        { url: 'http://189.126.111.132:8001/', description: 'Servidor de Produção' },
+        { url: 'http://localhost:8001/', description: 'Servidor de Produção' },
       ],
   
       tags: [
